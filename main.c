18d25e855f5e4021d652082df156edffb8e3c7f0
technomancer_lv <obalins@gmail.com>
2019-04-18 18:55:41 +0300

    pdf folder added for documentation files

--- main.c
+++ main.c
@@ -80,12 +80,14 @@
 unsigned char	DxStatus=0;
 #define	DxDriveSelected	0
 		
-unsigned char	DxArrayPointer=0;
-unsigned char 	DxArray[128];
+unsigned char 	DxArray[128];		//DX data buffer array, 128 bytes
+unsigned char	DxArrayPointer=0;	//Pointer for saving and reading data from array
+unsigned char	DxCommand=0;		//DX command received from controller. Used in state machine
+unsigned char	DxState=0;		//State machine state
 
 //Functions
-unsigned char RomRead(unsigned char TrkNum,unsigned char SecNum);
-unsigned char RomWrite(unsigned char TrkNum,unsigned char SecNum);
+unsigned char 	RomRead(unsigned char TrkNum,unsigned char SecNum);
+unsigned char 	RomWrite(unsigned char TrkNum,unsigned char SecNum);
 
 
 
@@ -94,11 +96,11 @@ int main()
 {
 	//INIT
 	//port init
-	//DX NACH.UST.  - input, pulled high
+	//DX SET (NACH.UST.)  - input, pulled high
 	DxSetPort|=(1<<DxSetPin);
 	DxSetDdr&=~(1<<DxSetPin);
 
-	//DX PUSK  - input, pulled high
+	//DX RUN (PUSK)  - input, pulled high
 	DxRunPort|=(1<<DxRunPin);
 	DxRunDdr&=~(1<<DxRunPin);
 
@@ -106,58 +108,60 @@ int main()
 	DxDiPort|=(1<<DxDiPin);
 	DxDiDdr&=~(1<<DxDiPin);
 
-	//DX data out pin - output, high
-	DxDoPort|=(1<<DxDoPin);
+	//DX data out pin - output, low
+	DxDoPort&=~(1<<DxDoPin);
 	DxDoDdr|=(1<<DxDoPin);
 
-	//DX TREB.PER.  - output, high
+	//DX IR (TREB.PEREDACHI).  - output, high
 	DxIrPort|=(1<<DxIrPin);
 	DxIrDdr|=(1<<DxIrPin);
 
-	//DX ZAVERSHENO  - output, high
+	//DX DONE (ZAVERSHENO)  - output, high
 	DxDonePort|=(1<<DxDonePin);
 	DxDoneDdr|=(1<<DxDonePin);
 
-	//DX VYVOD  - output, high
+	//DX OUTPUT (VYVOD)  - output, high
 	DxOutPort|=(1<<DxOutPin);
 	DxOutDdr|=(1<<DxOutPin);
 
-	//DX OSHIBKA  - output, high
+	//DX ERROR (OSHIBKA)  - output, high
 	DxErrPort|=(1<<DxErrPin);
 	DxErrDdr|=(1<<DxErrPin);
 
-	//DX TREB.PER.  - output, high
+	//DX SHIFT (SDVIG)  - output, high
 	DxShftPort|=(1<<DxShftPin);
 	DxShftDdr|=(1<<DxShftPin);
 
 
 	//UART init
 	//SPI-mem init
-	//SPI-DX init	
+	//SPI-DX init
 
 
 
-	_delay_ms(1000);	//Startup delay
+	_delay_ms(1000);		//Startup delay
+	DxDonePort&=~(1<<DxDonePin);	//
 
 	//========== MAIN LOOP ==========
+
+	while (1)
+	{}
+
 	while(1)
 	{
-		DxDonePort&=~(1<<DxDonePin);
-
 		if ((DxRunIn&(1<<DxRunPin))==0)
 		{
 			//Controller has initiated command transfer. Read command over SPI-DX.
-			DxFinishedPort|=(1<<DxFinishedPin);
+			DxDonePort|=(1<<DxDonePin);
 
 			//Read 8 bits
 			for (unsigned char i=0;i<8;i++)
 			{
-				_delay_us(10);
-				DxShiftPort&=~(1<<DxShiftPin);
-				_delay_us(10);
-				DxShiftPort|=(1<<DxShiftPin);
+				_delay_us(1);
+				DxShftPort&=~(1<<DxShftPin);
+				_delay_us(2);
+				DxShftPort|=(1<<DxShftPin);
 			}
-			unsigned char DxCommand=0;
 			
 			switch (DxCommand)
 			{
